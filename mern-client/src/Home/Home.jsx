032cbd6f8f47d794a3325ab8bfc100b4cdf30450
ayush2111 <130962034+ayush2111@users.noreply.github.com>
2024-07-06 16:16:13 +0530

    Update Home.jsx

--- mern-client/src/Home/Home.jsx
+++ mern-client/src/Home/Home.jsx
@@ -4,6 +4,10 @@ import BestSellerBooks from './BestSellerBooks'
 import FavBooks from './FavBooks'
 import PromoBanner from './PromoBanner'
 import OtherBooks from './OtherBooks'
+import Review from './Review'
+import BookCards from '../Components/BookCards';
+
+
 
 const Home = () => {
   return (
@@ -13,6 +17,7 @@ const Home = () => {
       <FavBooks/>
       <PromoBanner/>
       <OtherBooks/>
+      <Review/>
     </div>
   )
 }
