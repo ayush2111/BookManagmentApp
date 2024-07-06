f1be6ae1b4967d908543bba317449a678b54e12e
ayush2111 <130962034+ayush2111@users.noreply.github.com>
2024-07-06 16:11:57 +0530

    Update Shop.jsx

--- mern-client/src/Shop/Shop.jsx
+++ mern-client/src/Shop/Shop.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useEffect , useState}from 'react'
+import { Card } from 'flowbite-react';
 
 const Shop = () => {
+  const[books, setBooks] = useState([]);
+
+  useEffect( () => {
+    fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => setBooks(data));
+  }, [])
   return (
-    <div>Shop</div>
+    <div className='mt-28 px- lg:px24'>
+      <h2 className='text-5xl font-bold text-center'>All Books are Here</h2>
+    
+    <div className='grid gap-8 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1'>
+      {
+        books.map(book =>  <Card
+        >
+          <img src={book.image_url} alt="" className='h-96'/>
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <p>
+              {book.book_title}
+              </p>
+          </h5>
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+           <p> Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+         </p>
+          </p>
+
+          <button className='bg-blue-700 font-semibold text-white py-2 rounded'>Buy Now</button>
+        </Card>)
+      }
+    </div>
+    
+    </div>
   )
 }
 
