a1af42a422f847c9480954a387517e1b67ace440
ayush2111 <130962034+ayush2111@users.noreply.github.com>
2024-07-06 16:10:53 +0530

    Update App.jsx

--- mern-client/src/App.jsx
+++ mern-client/src/App.jsx
@@ -1,15 +1,20 @@
-
+import { useContext } from 'react'
 import './App.css'
 import { Outlet} from 'react-router-dom'
 import Navbar from './Components/Navbar'
+import { AuthContext } from './Context/AuthProvider'
+import FooterMain from './Components/FooterMain'
+
 
 function App() {
-
+  const {user} = useContext(AuthContext)
   return (
     <>
      <Navbar/>
-
+   <div className='min-h-screen'>
      <Outlet/>
+   </div>
+    <FooterMain/>
     </>
   )
 }
